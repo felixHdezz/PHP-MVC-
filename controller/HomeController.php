0236da042dd8c9f4b804896487680d0126835fcf
Félix Hernández Hernández <felix_hdezz95@hotmail.com>
2018-03-15 09:16:43 -0600

    Update HomeController.php

--- controller/HomeController.php
+++ controller/HomeController.php
@@ -3,12 +3,14 @@
 	{
 		public $connection = null;
 		public $adapter = null;
+		// Inicializa la conexión a la DB
 		function __construct() {
 			parent::__construct();
-	        $this->connection = new Connection();
-	        $this->adapter = $this->connection->dbConnection();
+	        	$this->connection = new Connection();
+	        	$this->adapter = $this->connection->dbConnection();
 		}
-
+		
+		// Función para mostrar las vistas
 		public function Index(){
 			$this->view();
 		}
