84e55c0f95ff07ff6ea3c8986e3fa8e8bc9d8011
Félix Hernández Hernández <felix_hdezz95@hotmail.com>
2018-03-15 09:12:38 -0600

    Update index.php

--- index.php
+++ index.php
@@ -1,13 +1,15 @@
 <?php 
-	//include config global
+	// Include config global
 	require_once('./config/global.php');
 	require_once('./core/Controller.php');
 	require_once('./core/LoadController.php');
 
+	// Si existe el controller
 	if(isset($_GET["controller"])){
 	    $controllerObj=loadController($_GET["controller"]);
 	    tiggerAction($controllerObj);
 	}else{
+	    // Cargar default controller
 	    $controllerObj=loadController(DEFAULT_CONTROLLER);
 	    tiggerAction($controllerObj);
 	}
