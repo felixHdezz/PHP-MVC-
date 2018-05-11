37ec6176d128e3225656861167ecd80a0c16c9ac
Félix Hernández Hernández <felix_hdezz95@hotmail.com>
2018-05-11 09:02:28 -0500

    Update Connection.php

--- core/Connection.php
+++ core/Connection.php
@@ -1,13 +1,15 @@
 <?php 
 	class Connection
 	{
+		// globals variables
 		public $DRIVER   = "";
 		public $HOST     = "";
 		public $USER     = "";
 		public $PASSWORD = "";
 		public $DATABASE = "";
 		public $CHARSET  = ""; 
-
+		
+		// Init construct
 		public function __construct() {
 			$dbConfig = include_once('config/database.php');
 			$this->DRIVER   = $dbConfig['driver'];
@@ -17,7 +19,8 @@
 			$this->DATABASE = $dbConfig['database'];
 			$this->CHARSET  = $dbConfig['charset'];
 		}
-
+		
+		// Connection to data base MySQL
 		public function dbConnection(){
 			if (isset($this->DRIVER) || $this->DRIVER == "mysql" && $this->DRIVER != null) {
 				$con = mysqli_connect($this->HOST, $this->USER, $this->PASSWORD, $this->DATABASE);
